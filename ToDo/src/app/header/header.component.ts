19d3d4a5149ef1a62805fd5ea28f8971f5275629
hanieh hosseini <119689812+haniehhoseini@users.noreply.github.com>
2023-11-18 19:52:34 +0330

    todo list

---
+++ ToDo/src/app/header/header.component.ts
@@ -0,0 +1,40 @@
+import { Component , Output ,EventEmitter} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  
+  textColor : string = 'blue';
+
+  list : any = '';
+
+  menuListItem: any [] = [];
+  errorItems: boolean[] = [];
+
+  @Output() mylist = new EventEmitter();
+
+  WriteMyList(write : any){
+    this.list =write.target.value;
+
+  }
+  OnClick(){
+    
+    this.mylist.emit(this.list);
+    this.menuListItem.push(this.list);
+    
+  }
+
+  deleteTodo(index: number):void {
+    this.menuListItem.splice(index, 1);
+  }
+
+  ErrorTodo(index: number):void{
+    this.errorItems[index] = !this.errorItems[index];
+  }
+}
