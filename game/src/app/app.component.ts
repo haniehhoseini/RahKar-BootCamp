4d1d16142ef0f4a382fa57ee09e9d74fc23e59bb
hanieh hosseini <119689812+haniehhoseini@users.noreply.github.com>
2023-12-20 17:27:47 +0330

    game website

--- game/src/app/app.component.ts
+++ game/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet ,FlexLayoutModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'game';
+  constructor(private router: Router){}
+  play(){
+    this.router.navigateByUrl('game');
+  }
 }
