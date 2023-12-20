595026dbc5c8c92b61555b6820dcad77c9ca93a5
hanieh hosseini <119689812+haniehhoseini@users.noreply.github.com>
2023-12-20 18:47:40 +0330

    game website

--- game/src/app/app.component.ts
+++ game/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet ,FlexLayoutModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
